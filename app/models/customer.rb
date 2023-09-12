8db70847a7d1c2409d1145a8a5b4d0497fbf2590
Nick Spencer <strangebrew202@gmail.com>
2023-09-12 12:21:42 -0600

    Add: association and test to reach from Customer through invoices: :items to :merchants, I haven't used this functionality before, but through testing in rails c the AR association apears to work

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :items, through: :invoices
+  has_many :merchants, through: :items
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it { should have_many(:invoices) }
-  it { should have_many(:items).through(:invoices)}
+  it { should have_many(:items).through(:invoices) }
+  it { should have_many(:merchants).through(:items) }
 end
