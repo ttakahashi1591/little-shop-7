5f27849ed057b59a45a9209a02dd1037ff65e787
Nick Spencer <strangebrew202@gmail.com>
2023-09-12 11:23:54 -0600

    Add: spec tests for US 1 and 2, route + controller + view for merchants/:id/dashboard added.

---
+++ spec/merchants/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant Dashboard", type: :feature do
+  before(:each) do
+    @merchant_1 = Merchant.create!(name: "Geoff's Goodies")
+  end
+  describe "As a merchant" do
+    describe "When i viist my dashboard (/merchants/:merchant_id/dashboard)" do
+      it "Then i see the name of my merchant" do
+
+        visit "/merchants/#{@merchant_1.id}/dashboard"
+
+        expect(page).to have_content(@merchant_1.name)
+
+      end
+
+      it "I also see a link to my items index /merchants/:id/items" do
+
+        visit "merchants/#{@merchant_1.id}/dashboard"
+
+        expect(page).to have_link("/merchants/#{@merchant_1.id}/items")
+      end
+
+      it "I also see a link to my invoices index /merchants/:id/invoices" do
+
+        visit "merchants/#{@merchant_1.id}/dashboard"
+
+        expect(page).to have_link("/merchants/#{@merchant_1.id}/invoices")
+      end
+    end
+  end
+end
