e013f2de405fcaaea72194b7f1d8cb4750301954
Robert deLaguna <rjdelaguna@yahoo.com>
2023-09-13 20:13:49 -0700

    feat and test: User Story 32

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,7 +4,3 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :customers
 end
-
-
-# merchant1.customers.select('customers.*, count(transactions.id) as transactions').joins(:transactions).where(transactions: { result: "success" }).group(:
-  # id).order('transactions desc')
