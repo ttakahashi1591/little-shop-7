892609bc14bca3ce7c8271f5f343f7e5691d9782
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-09-13 22:10:29 -0600

    feat: User Story 24 completed

--- spec/admin_merchants/index_spec.rb
+++ spec/admin_merchants/index_spec.rb
@@ -12,11 +12,23 @@ RSpec.describe "admin dashboard", type: :feature do
 
         visit "/admin/merchants"
 
-        within("#body") do 
+        within("#merchant-#{@merchant1.id}") do 
           expect(page).to have_content(@merchant1.name)
+        end
+
+        within("#merchant-#{@merchant2.id}") do 
           expect(page).to have_content(@merchant2.name)
+        end
+
+        within("#merchant-#{@merchant3.id}") do 
           expect(page).to have_content(@merchant3.name)
+        end
+          
+        within("#merchant-#{@merchant4.id}") do 
           expect(page).to have_content(@merchant4.name)
+        end
+
+        within("#merchant-#{@merchant5.id}") do
           expect(page).to have_content(@merchant5.name)
         end
       end
