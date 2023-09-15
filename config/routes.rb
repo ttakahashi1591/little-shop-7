d821fe6ce91a4da007cb042d0e74b21d1824c975
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-09-15 12:48:42 -0600

    feat: User Story 26 completed

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   get '/merchants/:id/dashboard', to: 'merchants#show'
   get "/merchants/:id/items", to: "merchant_items#index"
   get "/merchants/:merchant_id/items/:id", to: "merchant_items#show", as: :merchant_item
-
   get "/admin", to: "admin#index"
   get "/admin/merchants", to: "admin_merchants#index"
-  get "admin/merchants/:id", to: "admin_merchants#show"
+  get "/admin/merchants/:id", to: "admin_merchants#show"
+  get "/admin/merchants/:id/edit", to: "admin_merchants#edit"
+  patch "/admin/merchants/:id", to: "admin_merchants#update"
   get "/admin/invoices", to: "admin_invoices#index"
-  get "admin/invoices/:id", to: "admin_invoices#show"
+  get "/admin/invoices/:id", to: "admin_invoices#show"
 end
