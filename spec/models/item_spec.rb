4ed80e3957674d6923c0ed2785be51f74fb85425
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-19 10:31:33 -0600

    Setup data file, complete merchant index data

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Item, type: :model do
 
   describe "instance methods" do
     before :each do
-      @merchant = Merchant.create!(name: "Merchant")
+      @merchant = create(:merchant)
       @merchant1 = create(:merchant)
       @merchant2 = create(:merchant)
       @merchant3 = create(:merchant)
@@ -21,7 +21,7 @@ RSpec.describe Item, type: :model do
       @customer1 = Customer.create!(first_name: "John" , last_name: "Smith")
       
       @invoice1 = @customer1.invoices.create!(status: 1)
-      
+    
       @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 1, status: 2)
       @invoice_item2 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice1.id, quantity: 2, status: 2)
       @invoice_item3 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice1.id, quantity: 3, status: 2)
@@ -46,56 +46,7 @@ RSpec.describe Item, type: :model do
 
   describe "#class methods" do
     before :each do
-      @merchant1 = create(:merchant)
-      @merchant2 = create(:merchant)
-      @merchant3 = create(:merchant)
-      @merchant4 = create(:merchant)
-      @merchant5 = create(:merchant)
-      @merchant6 = create(:merchant)
-      
-      @item1 = create(:item, unit_price: 10, merchant_id: @merchant1.id)
-      @item2 = create(:item, unit_price: 50, merchant_id: @merchant2.id)
-      @item3 = create(:item, unit_price: 100, merchant_id: @merchant3.id)
-      @item4 = create(:item, unit_price: 200, merchant_id: @merchant4.id)
-      @item5 = create(:item, unit_price: 300, merchant_id: @merchant5.id)
-      @item6 = create(:item, unit_price: 400, merchant_id: @merchant6.id)
-      
-      @customer = Customer.create!(first_name: "John", last_name: "Smith")
-      @invoice1 = @customer.invoices.create!(status: 1, created_at: "2023-08-07")
-      @invoice2 = @customer.invoices.create!(status: 1, created_at: "2023-08-08")
-      @invoice3 = @customer.invoices.create!(status: 1, created_at: "2023-08-11")
-      @invoice4 = @customer.invoices.create!(status: 1, created_at: "2023-08-12")
-      @invoice5 = @customer.invoices.create!(status: 1, created_at: "2023-08-14")
-      @invoice6 = @customer.invoices.create!(status: 1, created_at: "2023-08-15")
-      @invoice7 = @customer.invoices.create!(status: 1, created_at: "2023-08-09")
-      @invoice8 = @customer.invoices.create!(status: 1, created_at: "2023-08-09")
-      @invoice9 = @customer.invoices.create!(status: 1, created_at: "2023-08-12")
-      @invoice10 = @customer.invoices.create!(status: 1, created_at: "2023-08-12")
-
-      
-      InvoiceItem.create!(item_id: @item6.id, invoice_id: @invoice6.id, quantity: 7, unit_price: 400, status: 2)
-      InvoiceItem.create!(item_id: @item5.id, invoice_id: @invoice5.id, quantity: 6, unit_price: 300, status: 2)
-      InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice4.id, quantity: 4, unit_price: 200, status: 2)
-      InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 3, unit_price: 100, status: 2)
-      InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 2, unit_price: 50, status: 2)
-      InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 1, unit_price: 10, status: 2)
-      
-      InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice7.id, quantity: 1, unit_price: 10, status: 2)
-      InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice8.id, quantity: 1, unit_price: 10, status: 2)
-      InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice9.id, quantity: 1, unit_price: 50, status: 2)
-      InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice10.id, quantity: 1, unit_price: 50, status: 2)
-
-      @invoice1.transactions.create!(result: 1)
-      @invoice2.transactions.create!(result: 1)
-      @invoice3.transactions.create!(result: 1)
-      @invoice3.transactions.create!(result: 1)
-      @invoice4.transactions.create!(result: 1)
-      @invoice5.transactions.create!(result: 1)
-      @invoice6.transactions.create!(result: 1)
-      @invoice7.transactions.create!(result: 1)
-      @invoice8.transactions.create!(result: 1)
-      @invoice9.transactions.create!(result: 1)
-      @invoice10.transactions.create!(result: 1)
+      load_test_data
     end
 
     describe "#self.enabled, self.disabled" do
