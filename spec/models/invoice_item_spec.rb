cb324da59f24b4a133d5704f4c86c7c2cf29df48
Nick Spencer <strangebrew202@gmail.com>
2023-09-20 15:39:39 -0600

    Add: #find_item and #find_invoice_items model tests to shore up our test covereage

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  before(:each) do
+    load_test_data
+  end
   it { should belong_to :invoice }
   it { should belong_to :item }
+
+  describe "instance methods" do
+    describe "#find_item" do
+      it "finds and returns item associated with and invoice_item" do
+        
+        expect(@invoice_item1.find_item).to eq(@item7)
+      end
+    end
+
+    describe "#find_invoice_items" do
+      it "returns all invoice_items associated with given invoice" do
+
+        expect(InvoiceItem.find_invoice_items(@invoice11)).to eq([@invoice_item4])
+      end
+    end
+  end
 end
