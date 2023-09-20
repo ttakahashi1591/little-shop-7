e490b9bc7c1003fa7098c75b0cc7de26a787a98f
Nick Spencer <strangebrew202@gmail.com>
2023-09-20 13:04:33 -0600

    Add: Upsplash integration, gem added to gemfile, initializer added to initializers for upsplash, API call added to layouts/application.html.erb to apply image from upsplash on all pages

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "3CraDLFCz7Kx6V7La6L71sjI_gVgzd26cDWDZQ1cgn0"
+  config.application_secret = "GEUfAdpa8riFiREcmIyCX7JH8ZreR9gt_9S-1wWTsBw"
+  config.application_redirect_uri = "https://your-application.com/oauth/callback"
+  config.utm_source = "alices_terrific_client_app"
+end
