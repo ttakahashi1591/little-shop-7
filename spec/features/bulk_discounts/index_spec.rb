3eda144804a4ff7899bbad62d54382d6f458d3bf
Nick Spencer <strangebrew202@gmail.com>
2023-09-21 14:23:52 -0600

    Add: start of US1 feature tests for bulk_discounts index page, migration to add bulk_discounts table that references merchant

---
+++ spec/features/bulk_discounts/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Bulk Discounts index page", type: :feature do
+  before(:each) do
+    load_test_data
+  end
+  describe "As a visitor" do
+    describe "When I visit /merchants/:merchant_id/bulk_discounts" do
+      it "I see all of my bulk discounts including their
+      percentage discount and quantity thresholds" do
+
+        visit "/merchants/#{@merchant1}/bulk_discounts"
+
+      end
+
+      it "And each bulk discount listed includes a link to its show page" do
+
+        visit "/merchants/#{@merchant1}/bulk_discounts"
+
+      end
+    end
+  end
+end
