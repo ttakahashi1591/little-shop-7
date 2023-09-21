9ba2b1550b7a7737cad24e50f4d623e6213976dc
Nick Spencer <strangebrew202@gmail.com>
2023-09-21 14:48:17 -0600

    Add: resources route for bulk_discounts nested in merchants routes, bulk_discounts_controller, bulk_discounts index

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -9,13 +9,18 @@ RSpec.describe "Bulk Discounts index page", type: :feature do
       it "I see all of my bulk discounts including their
       percentage discount and quantity thresholds" do
 
-        visit "/merchants/#{@merchant1}/bulk_discounts"
+        visit "/merchants/#{@merchant1.id}/bulk_discounts"
 
+        expect(page).to have_content("Threshold: #{@discount_1.threshold}")
+        expect(page).to have_content("Discount: #{@discount_1.discount}")
+
+        expect(page).to have_content("Threshold: #{@discount_2.threshold}")
+        expect(page).to have_content("Discount: #{@discount_2.discount}")
       end
 
       it "And each bulk discount listed includes a link to its show page" do
 
-        visit "/merchants/#{@merchant1}/bulk_discounts"
+        visit "/merchants/#{@merchant1.id}/bulk_discounts"
 
       end
     end
