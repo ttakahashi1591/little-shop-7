bfd30604dd75c7bc3bbdbae6c17f24a57c546def
Nick Spencer <strangebrew202@gmail.com>
2023-09-22 10:25:46 -0600

    feature test for edit bulk_discount actions

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -13,6 +13,31 @@ RSpec.describe "Bulk Discounts index page", type: :feature do
         expect(page).to have_content("Threshold: #{@discount_1.threshold}")
         expect(page).to have_content("Discount: #{@discount_1.discount}")
       end
+
+      it "Then I see a link to edit the bulk discount
+      When I click this link
+      Then I am taken to a new page with a form to edit the discount
+      And I see that the discounts current attributes are pre-poluated in the form
+      When I change any/all of the information and click submit
+      Then I am redirected to the bulk discount's show page
+      And I see that the discount's attributes have been updated" do
+
+        visit "/merchants/#{@merchant1.id}/bulk_discounts/#{@discount_1.id}"
+
+        click_on "Edit"
+
+        expect(page).to have_content("Threshold: #{@discount_1.threshold}")
+        expect(page).to have_content("Discount: #{@discount_1.discount}")
+
+        fill_in :threshold, with: "2"
+        fill_in :discount, with: "5"
+        click_on "Submit"
+
+        within(".discount_id_#{@discount_1.id}") do
+          expect(page).to have_content("Threshold: 2")
+          expect(page).to have_content("Discount: 5")
+        end
+      end
     end
   end
 end
