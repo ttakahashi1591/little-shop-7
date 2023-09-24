c2b9180e26bb9eba35234d125a15f24a93a973dd
Nick Spencer <strangebrew202@gmail.com>
2023-09-24 08:00:13 -0600

    Add: discount link only shows if a discount was applied to an invoice_item now

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -5,7 +5,7 @@ class MerchantInvoicesController < ApplicationController
 
   def show
     @invoice = Invoice.find(params[:id])
-    @discounts = Merchant.find(params[:merchant_id]).bulk_discounts
     @invoice_items = InvoiceItem.find_invoice_items(@invoice)
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
