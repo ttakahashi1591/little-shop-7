7fe7df0f1c00c2412638920d9c89ee75c39d36bc
Nick Spencer <strangebrew202@gmail.com>
2023-09-24 20:01:56 -0600

    Add: Test case for #revenue_with_discounts to test method function returns the correct total revenue when merchant has no applicable discounts

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -87,6 +87,24 @@ RSpec.describe Invoice, type: :model do
 
           expect(invoice1.revenue_with_discounts).to eq(13.50)
         end
+
+        it "still returns the non discounted total if no items qualify for bulk_discounts" do
+          merchant1 = Merchant.create!(name: "Merchant")
+        
+          item1 = merchant1.items.create!(name: "Item 1", description: "First Item", unit_price: 10)
+          item2 = merchant1.items.create!(name: "Item 2", description: "Second Item", unit_price: 20)
+          item3 = merchant1.items.create!(name: "Item 3", description: "Third Item", unit_price: 30)
+          
+          customer1 = Customer.create!(first_name: "John" , last_name: "Smith")
+         
+          invoice1 = customer1.invoices.create!(status: 1)
+          
+          invoice_item1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 1, unit_price: 10, status: 2)
+          invoice_item2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice1.id, quantity: 2, unit_price: 20, status: 2)
+          invoice_item3 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice1.id, quantity: 3, unit_price: 30, status: 2)
+  
+          expect(invoice1.revenue_with_discounts).to eq(1.40)
+        end
       end
     end
   end
