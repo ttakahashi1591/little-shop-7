6b77ef4f17240d731beb5b9be1cbbba272cf7512
Nick Spencer <strangebrew202@gmail.com>
2023-09-25 15:40:08 -0600

    Add: #can_be_modified? method to BulkDiscount in order to start on first extension

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -4,4 +4,19 @@ class BulkDiscount < ApplicationRecord
 
   validates :threshold, numericality: { only_integer: true }
   validates :discount, numericality: { only_integer: true }
+  
+  def can_be_modified?
+    no_modify_array = BulkDiscount.find_by_sql(
+      "select bulk_discounts.id from bulk_discounts
+        inner join merchants on merchants.id = bulk_discounts.merchant_id
+        inner join items on items.merchant_id = merchants.id
+        inner join invoice_items on invoice_items.item_id = items.id
+        inner join invoices on invoices.id = invoice_items.invoice_id
+      where invoices.status = 2
+      and invoice_items.quantity >= bulk_discounts.threshold
+      group by bulk_discounts.id;"
+    ).pluck('id')
+
+    no_modify_array.include?(self.id) ? false : true
+  end
 end
